d0a4ac5078060c8eeace17eadf222c9995ce1ff4
jinn-ie <104821183+jinn-ie@users.noreply.github.com>
2023-01-03 16:15:55 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,17 @@
+pitch=list(map(int,input().split()))
+
+if (pitch[0]==1):
+    check='ascending'
+    for i in range(1,7):
+        if (pitch[i]+1!=pitch[i+1]):
+            check='mixed'
+            break
+elif (pitch[0]==8):
+    check='descending'
+    for i in range(1,7):
+        if (pitch[i]-1!=pitch[i+1]):
+            check='mixed'
+            break    
+else: check='mixed'
+
+print(check)
