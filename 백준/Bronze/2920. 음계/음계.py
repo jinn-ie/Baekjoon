804cf2792957d302a11caa56a1ecfdadbdd80178
jinn-ie <104821183+jinn-ie@users.noreply.github.com>
2023-01-03 16:17:12 +0900

    Update 음계.py

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -2,7 +2,7 @@ pitch=list(map(int,input().split()))
 
 if (pitch[0]==1):
     check='ascending'
-    for i in range(1,7):
+    for i in range(1,7):                    # feedback: for문 없이 sorted 이용하면 간단함
         if (pitch[i]+1!=pitch[i+1]):
             check='mixed'
             break
