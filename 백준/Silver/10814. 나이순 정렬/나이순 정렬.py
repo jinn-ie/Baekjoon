3dc08852d2593382275792e4d09fd23d702c0439
jinn-ie <104821183+jinn-ie@users.noreply.github.com>
2023-01-05 16:16:31 +0900

    [Silver V] Title: 나이순 정렬, Time: 260 ms, Memory: 54468 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,12 +1,16 @@
-judge=[]
+import sys
+input=sys.stdin.readline
+
+judge=[]    # dict는 중복값 처리 불가
 
 n=int(input())
 
 for i in range(n):
-    person=list(input().split())
+    person=list(input().rstrip().split())
     judge.append(person)
 
 s_judge=sorted(judge,key=lambda x:int(x[0]))
+# int로 비교 안하면 9>10이 됨
 
 for age,name in s_judge:
     print(age,name)
