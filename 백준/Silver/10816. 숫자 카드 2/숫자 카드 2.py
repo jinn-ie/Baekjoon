76fcaf4ff9b2c1d744f1e5f14afa7661dcc744cd
jinn-ie <104821183+jinn-ie@users.noreply.github.com>
2023-01-16 21:05:01 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 788 ms, Memory: 134320 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+input()
+card_list=list(input().split())
+input()
+num_list=list(input().split())
+
+                        # 시간초과 해결방안
+c=Counter(card_list)    # 요소,빈도수 쌍의 딕셔너리 생성
+
+for num in num_list:
+    if num in c:
+        print(c[num],end=' ')
+    else:
+        print(0,end=' ')
