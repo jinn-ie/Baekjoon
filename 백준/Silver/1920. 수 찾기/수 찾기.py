79531d1265efbc5e9b156c5584081516fc6e5359
jinn-ie <104821183+jinn-ie@users.noreply.github.com>
2023-01-16 20:48:40 +0900

    [Silver IV] Title: 수 찾기, Time: 484 ms, Memory: 45924 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,28 @@
+def binary(n_list,num,start,end):
+    if (start>end):
+        return False
+    
+    curr=(start+end)//2
+    
+    if (num<n_list[curr]):
+        return binary(n_list,num,start,curr-1)
+    elif (num==n_list[curr]):
+        return True
+    elif (num>n_list[curr]):
+        return binary(n_list,num,curr+1,end)
+  
+    
+def main():
+    n=int(input())
+    n_list=list(map(int,input().split()))
+    m=int(input())
+    m_list=list(map(int,input().split()))
+
+    n_list.sort()
+    for i in range(m):
+        if(binary(n_list,m_list[i],0,len(n_list)-1)):
+            print(1)
+        else:
+            print(0)
+            
+main()
