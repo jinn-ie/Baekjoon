646377190fe87f4452b4b86e794087c5315e61e1
jinn-ie <104821183+jinn-ie@users.noreply.github.com>
2023-01-29 18:59:27 +0900

    [Silver II] Title: 랜선 자르기, Time: 460 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -5,12 +5,12 @@ for i in range(k):
      lans.append(int(input()))
 
 res=0
-start=0
-end=max(lans)       # 최대길이로 기준잡아야함!
+start=0                         # 이분탐색!!!
+end=max(lans)                   # 최대길이로 기준잡아야함!
 
-while(start!=end or res<n):
+while(start!=end or res<n):     # 랜선 n개 이상이어야 탈출
      res=0
-     length=(end+start+1)//2
+     length=(end+start+1)//2    # 자연수 몫이라서 +1 해야함
      for lan in lans:
           res+=lan//length
      if res<n:
