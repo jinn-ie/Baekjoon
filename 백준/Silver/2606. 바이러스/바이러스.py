e8387cb0fa73c1468fbbda202c94817475210452
jinn-ie <104821183+jinn-ie@users.noreply.github.com>
2023-09-02 00:14:41 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,29 +1,27 @@
 import sys
 input=sys.stdin.readline
 
-def dfs(first,computers,networks):
+computers=[]
+networks=[]
+
+def dfs(first):                                 # dfs-재귀로 구현
     if computers[first-1]==False:
         computers[first-1]=True
-        for network in networks:
+        for network in networks:                # 연결 탐색
             if network[0]==first:
-                dfs(network[1],computers,networks)
+                dfs(network[1])
+                del network                     # 계산이 끝나면 삭제
+            elif network[1]==first:             # 역연결도 고려
+                dfs(network[0])
                 del network
-            elif network[1]==first:
-                dfs(network[0],computers,networks)
-                del network
-        
 
 def main():
-    computer=int(input())
+    c=int(input())
     n=int(input())
-    
-    computers=[False]*computer  # 바이러스 감염 여부 체크리스트
-    networks=[]
-    
-    for i in range(n):
-        networks.append(list(map(int,input().split())))
+    for _ in range(c):  computers.append(False)  # 바이러스 감염 여부 체크리스트
+    for _ in range(n):  networks.append(list(map(int,input().split())))
         
-    dfs(1,computers,networks)
+    dfs(1)
     
     print(computers.count(True)-1)
     
