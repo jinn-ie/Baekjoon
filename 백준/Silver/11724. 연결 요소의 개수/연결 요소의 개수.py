34ea0b474a33d7f5f1798ce3f38b0d0dd5b7926c
Seojin Lee <104821183+jinn-ie@users.noreply.github.com>
2025-03-17 16:00:08 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 544 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -3,11 +3,9 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 group = [i for i in range(n)]
-edges = []
 
 for _ in range(m):
     u, v = map(int, input().split())
-    edges.append([u, v])
 
     if group[u-1] != group[v-1]:
         g = group[v-1]
