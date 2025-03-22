634d579cb0fad3ade06ab8e88445e7738f044efd
Seojin Lee <104821183+jinn-ie@users.noreply.github.com>
2025-03-22 18:21:29 +0900

    [Silver III] Title: 주유소, Time: 104 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -4,15 +4,14 @@ input = sys.stdin.readline
 n = int(input())
 km = list(map(int, input().split()))
 price = list(map(int, input().split()))
-price.pop()
 
 res = 0
+min_price = price[0]
 
-while price:
-    idx = price.index(min(price))
-    for i in range(len(price) - 1, idx - 1, -1):
-        res += km[i] * price[idx]
-        km.pop()
-        price.pop()
+for i in range(n - 1):
+    if price[i] < min_price:
+        min_price = price[i]
+
+    res += min_price * km[i]
 
 print(res)
