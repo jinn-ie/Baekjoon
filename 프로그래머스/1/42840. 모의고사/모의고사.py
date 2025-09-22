44693fb26b1b1ca470c155a1e0ce47dbe59f2158
Seojin Lee <104821183+jinn-ie@users.noreply.github.com>
2025-09-22 16:54:36 +0900

    [level 1] Title: 모의고사, Time: 2.22 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,25 +1,21 @@
 def solution(answers):
     
     answer = []
-    
     score = [0, 0, 0]
     
-    one = [1, 2, 3, 4, 5] * 2000
-    two = [2, 1, 2, 3, 2, 4, 2, 5] * 1250
-    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 1000
+    # 배열 연장 (최대 10,000문제)
+    p1 = [1, 2, 3, 4, 5] * 2000
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5] * 1250
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 1000
     
+    # 사람별 맞힌 문제 개수 체크
     for i in range(len(answers)):
-        if answers[i] == one[i]:
-            score[0] += 1
-        if answers[i] == two[i]:
-            score[1] += 1
-        if answers[i] == three[i]:
-            score[2] += 1
-    
-    max_score = max(score)
+        if answers[i] == p1[i]: score[0] += 1
+        if answers[i] == p2[i]: score[1] += 1
+        if answers[i] == p3[i]: score[2] += 1
     
     for i in range(3):
-        if score[i] == max_score:
-            answer.append(i + 1)
+        if score[i] == max(score):  # 최고점이면
+            answer.append(i + 1)    # 정답 배열에 추가
     
     return answer
