c2b4c2f7a37a8f43e1cf9a125e1d67c7abf7dafc
Seojin Lee <104821183+jinn-ie@users.noreply.github.com>
2025-09-29 15:40:41 +0900

    [level 2] Title: 리코쳇 로봇, Time: 26.00 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -2,6 +2,7 @@ def solution(board):
     
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
+    
     len_x, len_y = len(board[0]), len(board)
     distance = [[99999999] * len_x for _ in range(len_y)]
     
@@ -12,24 +13,25 @@ def solution(board):
     queue = [[r[0], r[1]]]
     distance[r[0]][r[1]] = 0
     
+    # BFS
     while queue:
         yy, xx = queue[0][0], queue[0][1]
         del queue[0]
+        dis = distance[yy][xx]
 
-        for i in range(4):
+        for i in range(4):  # 상하좌우
             y, x = yy, xx
-            dis = distance[y][x]
             
-            while True:
+            while True:     # 벽, 장애물 만날때까지
                 if x + dx[i] >= len_x or x + dx[i] < 0: break
                 if y + dy[i] >= len_y or y + dy[i] < 0: break
-                if board[y + dy[i]][x + dx[i]] == 'D': break
+                if board[y + dy[i]][x + dx[i]] == 'D':  break
                 
-                y += dy[i]
+                y += dy[i]  # 미끄러지기
                 x += dx[i]
                 
-            if distance[y][x] > dis + 1:
-                distance[y][x] = dis + 1
+            if distance[y][x] > dis + 1:    # 해당 칸에 최소 이동횟수이면
+                distance[y][x] = dis + 1    # 이동 횟수 갱신
                 queue.append([y, x])
                     
                 
